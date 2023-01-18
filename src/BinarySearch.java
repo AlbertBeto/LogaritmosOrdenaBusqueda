fde76df49ee508915794e397b8d4bcfc3037e090
Beto <beto@beto.cat>
2023-01-18 17:29:20 +0100

    Solucionado el BinarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,21 +3,22 @@ public class BinarySearch {
     public static void BinarySearch(int[]array, int key, int low, int high){
 
 
-        int mid= (low+high)/2;
+
 boolean salida=false;
 
-while(salida=false) {
-
+while(salida==false) {
+    int mid= (low+high)/2;
     if (array[mid] == key) {
         salida = true;
         System.out.println("Se ha encontrado el valor buscado en la posición " + mid + " del array.");
-    } else if (array[mid] < key) {
-        high = mid - 1;
-    } else if (array[mid] > key) {
-        low = mid + 1;
     } else if (low==mid || high==mid) {
         salida=true;
         System.out.println("No se ha encontrado el valor dentro del array");
+    } else if (array[mid] < key) {
+        low = mid + 1;
+    } else if (array[mid] > key) {
+        high = mid - 1;
+
     }
 
 //while
