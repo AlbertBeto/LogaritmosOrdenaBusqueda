9bc626a5e9588552e4dc2debc44e8a2e7bcbddc5
Beto <beto@beto.cat>
2023-01-27 01:56:34 +0100

    Guia solucionada pero en MAin

--- src/GuiaTelefonica.java
+++ src/GuiaTelefonica.java
@@ -1,33 +1,34 @@
 public class GuiaTelefonica {
-    String[] teleguia = new String[5];
-    EntradaTelefono uno = new EntradaTelefono("Albert", 656777777);
-    EntradaTelefono dos = new EntradaTelefono("Edu", 656888888);
-    EntradaTelefono tres = new EntradaTelefono("Mai", 656222222);
-    EntradaTelefono cuatro = new EntradaTelefono("Mo", 656555555);
-    EntradaTelefono cinco = new EntradaTelefono("Bro", 656666666);
-    teleguia[0] = uno;
-    teleguia[1] = dos;
-    teleguia[2] = tres;
-    teleguia[3] = cuatro;
-    teleguia[4] = cinco;
-    public  void guiaTelefonica() {
 
 
+    public static void main(String[] args) {
+        EntradaTelefono teleguia[] = new EntradaTelefono[5];
+        teleguia[0]= new EntradaTelefono("Albert", 656777777);
+        teleguia[1] = new EntradaTelefono("Edu", 656888888);
+        teleguia[2] = new EntradaTelefono("Mai", 656222222);
+        teleguia[3] = new EntradaTelefono("Mo", 656555555);
+        teleguia[4] = new EntradaTelefono("Bro", 656666666);
+
+        for(int i=0;i< teleguia.length;i++){
+
+                    System.out.println(teleguia[i].getNombre()+" "+teleguia[i].getNumero());
+                          System.out.println();
+        }
     }
-
-
+/*
     public int busqueda(String nombre) {
 
-String buscador;
+        String buscador;
+
         for (int i=0;i< teleguia.length;i++) {
             buscador=teleguia[i];
-            if (nombre == teleguia[i].getNombre() ){
+            if (nombre == EntradaTelefono[i].getNombre() ){
 
             }
 
         }
         return 1;
     }
-
+*/
 
 }
